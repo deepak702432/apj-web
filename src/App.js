99196d306cf6aa0b141e775071fcbc98f275d858
deepak702432 <152056458+deepak702432@users.noreply.github.com>
2023-11-27 08:46:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Layout from './Components/Layout';
+//import Layout from './Componentens/Layout';
+// import Navbar from './Componentens/Navbar';
+//import Navbar from './Componentens/Navbar';
+// import Navbar from './Componentens/Navbar';
+ import Prenav from './Components/Prenav';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import About from './Componentens/About';
+// import Home from './Componentens/Home';
+ import Footer from './Components/Footer';
+
+function App() {
+  return (
+<>
+
+
+<Prenav/>
+<Layout/>
+
+<Footer/>
+
+
+</>
+  );
+}
+
+export default App;
